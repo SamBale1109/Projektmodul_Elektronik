b4a0faf2a87e27c7f9cd1c30db5e74d3f9ebc1d4
SamBale1109 <67586126+SamBale1109@users.noreply.github.com>
2025-07-01 11:06:17 +0200

    Add files via upload

---
+++ Kamera/zustandserkennung/zustandserkennung/yolov8_obb_train.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+from pathlib import Path
+import os
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+home = Path.home()
+# Load a model
+prev_trained_model_path = home / "runs" / "obb" / "train6" / "weights"/ "last.pt"
+model = YOLO(str(prev_trained_model_path))  # load a pretrained model statndart = "yolov8n-obb.pt"
+
+model_path = home / "Downloads" / "boiling_detection.v1i.yolov8-obb" / "data.yaml"
+print(f"\n\npath = {model_path}\n\n")
+
+# Train the model
+results = model.train(data=str(model_path), epochs=200, imgsz=640)
